3045f2655e634cf4ae57752deb5a79585bc5f40a
Aayushi Kumar <akumar4@seattleu.edu>
2022-02-08 12:25:45 -0800

    Added RollDice_InValid_Roll_1_Dice_0_Should_Return_Zero() in DiceHelperUnitTest.cs

--- UnitTests/Helpers/DiceHelperUnitTest.cs
+++ UnitTests/Helpers/DiceHelperUnitTest.cs
@@ -100,5 +100,19 @@ namespace UnitTests.Helpers
             Assert.AreEqual(0, result);
         }
 
+        [Test]
+        public void RollDice_InValid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(0, 1);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
+
     }
 }
